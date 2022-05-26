1edde041a71161fddc0f51949207afbe07b126e3
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2022-05-26 12:50:05 +0200

    fixed typescript error

--- components/NavBar/forIndex/Kde/KdeMobile.js
+++ components/NavBar/forIndex/Kde/KdeMobile.js
@@ -1,7 +1,6 @@
 import { Text, Button } from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
-import { isPropertySignature } from "typescript";
 export default function KdeMobile(props) {
   console.log(window.innerHeight);
   console.log(window.innerWidth);
